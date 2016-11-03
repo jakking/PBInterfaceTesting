8f03a822947125261454ce155cb19bf5cc1222c5
apmattis <amattis21@gmail.com>
2016-11-02 21:10:17 -0500

    created files with 10 and 10000 records

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -20,55 +20,46 @@ public class Testing {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         byte[] buffer = new byte[1000];
         CodedOutputStream output = CodedOutputStream.newInstance(stream, buffer.length);
-
-        record.nextObject();
-
-
-
         GroceryQueryProvider.InventoryDetails.Builder query = GroceryQueryProvider.InventoryDetails.newBuilder();
 
-        query.setStoreID(record.getStoreID());
-        query.setDate(record.getDate());
-        query.setProductName(record.getProductName());
-        query.setAmount(record.getAmount());
-        query.setRecordID(record.getRecordID());
-        query.setUnits(record.getUnits());
+//        String fout = "src/resources/records10.txt";
+        try{
+//            FileWriter fw = new FileWriter(fout);
+//            BufferedWriter bw = new BufferedWriter(fw);
 
-        query.build().writeTo(output);
+            for(int i = 0; i < 10; i++){
+                record.nextObject();
+                query.setStoreID(record.getStoreID());
+                query.setDate(record.getDate());
+                query.setProductName(record.getProductName());
+                query.setAmount(record.getAmount());
+                query.setRecordID(record.getRecordID());
+                query.setUnits(record.getUnits());
 
+                query.build().writeTo(output);
+//                bw.write("RecordID: " + record.getRecordID() + "\n" +
+//                        "StoreID: " + record.getStoreID() + "\n" +
+//                        "Date: " + record.getDate() + "\n" +
+//                        "Amount: " + record.getAmount() + "\n\n");
+
+            }
+            output.flush();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void testingReads() throws IOException {
-        String fout = "src/resources/records10.txt";
-        GroceryFuzzGenerator gf = new GroceryFuzzGenerator();
-        try{
-            FileWriter fw = new FileWriter(fout);
-            BufferedWriter bw = new BufferedWriter(fw);
 
-            for(int i = 0; i < 10; i++){
-                gf.nextObject();
-                bw.write("RecordID: " + gf.getRecordID() + "\n" +
-                         "StoreID: " + gf.getStoreID() + "\n" +
-                         "Date: " + gf.getDate() + "\n" +
-                         "Amount: " + gf.getAmount() + "\n\n");
-
-            }
-
-            bw.close();
-
-        }catch(IOException e) {
-            e.printStackTrace();
-        }
-
-//        FileInputStream output = new FileInputStream("src/resources/codedOutput.txt");
-//        GroceryQueryProvider.InventoryDetails inv = GroceryQueryProvider.InventoryDetails.parseFrom(output);
-//        System.out.println(inv.getStoreID());
-//        System.out.println(inv.getDate());
-//        System.out.println(inv.getProductName());
-//        System.out.println(inv.getAmount());
-//        System.out.println(inv.getRecordID());
-//        System.out.println(inv.getUnits());
+        FileInputStream output = new FileInputStream("src/resources/codedOutput.txt");
+        GroceryQueryProvider.InventoryDetails inv = GroceryQueryProvider.InventoryDetails.parseFrom(output);
+        System.out.println(inv.getStoreID());
+        System.out.println(inv.getDate());
+        System.out.println(inv.getProductName());
+        System.out.println(inv.getAmount());
+        System.out.println(inv.getRecordID());
+        System.out.println(inv.getUnits());
 
 
 
