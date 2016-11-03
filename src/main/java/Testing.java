19567c3e1f17898705d06b7748280ba4c644185e
apmattis <amattis21@gmail.com>
2016-11-02 20:29:09 -0500

    created files with 10 and 10000 records

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -1,8 +1,8 @@
 import com.google.protobuf.CodedOutputStream;
-import com.ociweb.pronghorn.pipe.Pipe;
-import com.ociweb.pronghorn.pipe.PipeConfig;
-import com.ociweb.pronghorn.pipe.RawDataSchema;
-import com.ociweb.pronghorn.pipe.util.StreamRegulator;
+//import com.ociweb.pronghorn.pipe.Pipe;
+//import com.ociweb.pronghorn.pipe.PipeConfig;
+//import com.ociweb.pronghorn.pipe.RawDataSchema;
+//import com.ociweb.pronghorn.pipe.util.StreamRegulator;
 import generators.GroceryFuzzGenerator;
 import org.junit.Test;
 import pb_grocery.GroceryQueryProvider;
@@ -19,7 +19,7 @@ public class Testing {
         GroceryFuzzGenerator record = new GroceryFuzzGenerator();
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         byte[] buffer = new byte[1000];
-        CodedOutputStream output = new CodedOutputStream.newInstance(stream, buffer);
+        CodedOutputStream output = CodedOutputStream.newInstance(stream, buffer.length);
 
         record.nextObject();
 
@@ -40,14 +40,35 @@ public class Testing {
 
     @Test
     public void testingReads() throws IOException {
-        FileInputStream output = new FileInputStream("src/resources/codedOutput.txt");
-        GroceryQueryProvider.InventoryDetails inv = GroceryQueryProvider.InventoryDetails.parseFrom(output);
-        System.out.println(inv.getStoreID());
-        System.out.println(inv.getDate());
-        System.out.println(inv.getProductName());
-        System.out.println(inv.getAmount());
-        System.out.println(inv.getRecordID());
-        System.out.println(inv.getUnits());
+        String fout = "src/resources/records10.txt";
+        GroceryFuzzGenerator gf = new GroceryFuzzGenerator();
+        try{
+            FileWriter fw = new FileWriter(fout);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            for(int i = 0; i < 10; i++){
+                gf.nextObject();
+                bw.write("RecordID: " + gf.getRecordID() + "\n" +
+                         "StoreID: " + gf.getStoreID() + "\n" +
+                         "Date: " + gf.getDate() + "\n" +
+                         "Amount: " + gf.getAmount() + "\n\n");
+
+            }
+
+            bw.close();
+
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+
+//        FileInputStream output = new FileInputStream("src/resources/codedOutput.txt");
+//        GroceryQueryProvider.InventoryDetails inv = GroceryQueryProvider.InventoryDetails.parseFrom(output);
+//        System.out.println(inv.getStoreID());
+//        System.out.println(inv.getDate());
+//        System.out.println(inv.getProductName());
+//        System.out.println(inv.getAmount());
+//        System.out.println(inv.getRecordID());
+//        System.out.println(inv.getUnits());
 
 
 
--- src/main/java/generators/GroceryFuzzGenerator.java
+++ src/main/java/generators/GroceryFuzzGenerator.java
@@ -1,5 +1,8 @@
 package generators;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -14,17 +17,36 @@ public class GroceryFuzzGenerator {
     private int RecordID;
     private String units = "units";
     private Random rnd;
+    private String string_date = "2-November-2016";
+
 
     public GroceryFuzzGenerator(){
         rnd = new Random();
         this.RecordID = 0;
     }
 
+    public GroceryFuzzGenerator(int storeID, long date, String productName, int amount, int recordID, String units) {
+        this.StoreID = storeID;
+
+    }
+
     public void nextObject(){
-        this.StoreID = rnd.nextInt();
-        this.date = rnd.nextLong();
-        this.amount = rnd.nextInt();
+        this.StoreID = rnd.nextInt(Integer.SIZE - 1);
+        this.date = setDate();
+        this.amount = rnd.nextInt(Integer.SIZE - 1);
         this.RecordID++;
+    }
+
+    private long setDate() {
+        SimpleDateFormat f = new SimpleDateFormat("dd-MMM-yyyy");
+        try {
+            Date d = f.parse(string_date);
+            long milliseconds = d.getTime();
+            date = milliseconds;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return getDate();
     }
 
     public int getStoreID() {
